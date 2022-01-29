868a56ac9b19ee409258f6a8f0dc6de4741f9f89
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-01-29 18:52:24 +0100

    dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [UsersModule]
+  imports: [
+      UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'heroku_59fc92bfc06b38b',
+      port: 3306,
+      username: 'bd30751719dba5',
+      database: 'ffa93e92624d8e1',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+  ]
 })
 export class AppModule {}
