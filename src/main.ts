707955b8faf56a53c3d9d706cc6d8a03835dca04
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-01-29 13:49:07 +0100

    build API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+      .setTitle('Crud Users')
+      .setDescription('The description')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
