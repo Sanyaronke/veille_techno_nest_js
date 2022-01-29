11704181e7ddaec62b0d4b02e28143ac7ffcee7a
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-01-29 18:57:39 +0100

    df

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import {TypeOrmModule} from "@nestjs/typeorm";
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'heroku_59fc92bfc06b38b',
-      port: 3306,
       username: 'bd30751719dba5',
       database: 'ffa93e92624d8e1',
       entities: ["dist/**/*.entity{.ts,.js}"],
